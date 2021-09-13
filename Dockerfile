schacon/directus

commit 7186c4e3810b751018f0c09a22481b3a58767901

Allow custom CLI commands to be added by extensions (#7675)
* Fix typescript errors in shared package

* Hooks for adding custom CLI commands

* Add CLI hooks to documentation

api/src/cli/index.ts
@@ -2,8 +2,10 @@
 
 /* eslint-disable no-console */
 
-import { program } from 'commander';
+import { Command } from 'commander';
 import start from '../start';
+import { emitAsyncSafe } from '../emitter';
+import { initializeExtensions, registerExtensionHooks } from '../extensions';
 import bootstrap from './commands/bootstrap';
 import count from './commands/count';
 import dbInstall from './commands/database/install';
@@ -15,61 +17,78 @@ import usersPasswd from './commands/users/passwd';
 
 const pkg = require('../../package.json');
 
-program.name('directus').usage('[command] [options]');
-program.version(pkg.version, '-v, --version');
+if (require.main === module) {
+	createCli()
+		.then((program) => program.parseAsync(process.argv))
+		.catch((err) => {
+			console.error(err);
+			process.exit(1);
+		});
+}
 
-program.command('start').description('Start the Directus API').action(start);
-program.command('init').description('Create a new Directus Project').action(init);
+export async function createCli(): Promise<Command> {
+	const program = new Command();
 
-const dbCommand = program.command('database');
-dbCommand.command('install').description('Install the database').action(dbInstall);
-dbCommand
-	.command('migrate:latest')
-	.description('Upgrade the database')
-	.action(() => dbMigrate('latest'));
-dbCommand
-	.command('migrate:up')
-	.description('Upgrade the database')
-	.action(() => dbMigrate('up'));
-dbCommand
-	.command('migrate:down')
-	.description('Downgrade the database')
-	.action(() => dbMigrate('down'));
+	await initializeExtensions();
+	registerExtensionHooks();
 
-const usersCommand = program.command('users');
+	await emitAsyncSafe('cli.init.before', { program });
 
-usersCommand
-	.command('create')
-	.description('Create a new user')
-	.option('--email <value>', `user's email`)
-	.option('--password <value>', `user's password`)
-	.option('--role <value>', `user's role`)
-	.action(usersCreate);
+	program.name('directus').usage('[command] [options]');
+	program.version(pkg.version, '-v, --version');
 
-usersCommand
-	.command('passwd')
-	.description('Set user password')
-	.option('--email <value>', `user's email`)
-	.option('--password <value>', `user's new password`)
-	.action(usersPasswd);
+	program.command('start').description('Start the Directus API').action(start);
+	program.command('init').description('Create a new Directus Project').action(init);
 
-const rolesCommand = program.command('roles');
-rolesCommand
-	.command('create')
-	.description('Create a new role')
-	.option('--role <value>', `name for the role`)
-	.option('--admin', `whether or not the role has admin access`)
-	.action(rolesCreate);
+	const dbCommand = program.command('database');
+	dbCommand.command('install').description('Install the database').action(dbInstall);
+	dbCommand
+		.command('migrate:latest')
+		.description('Upgrade the database')
+		.action(() => dbMigrate('latest'));
+	dbCommand
+		.command('migrate:up')
+		.description('Upgrade the database')
+		.action(() => dbMigrate('up'));
+	dbCommand
+		.command('migrate:down')
+		.description('Downgrade the database')
+		.action(() => dbMigrate('down'));
 
-program.command('count <collection>').description('Count the amount of items in a given collection').action(count);
+	const usersCommand = program.command('users');
 
-program
-	.command('bootstrap')
-	.description('Initialize or update the database')
-	.option('--skipAdminInit', 'Skips the creation of the default Admin Role and User')
-	.action(bootstrap);
+	usersCommand
+		.command('create')
+		.description('Create a new user')
+		.option('--email <value>', `user's email`)
+		.option('--password <value>', `user's password`)
+		.option('--role <value>', `user's role`)
+		.action(usersCreate);
 
-program.parseAsync(process.argv).catch((err) => {
-	console.error(err);
-	process.exit(1);
-});
+	usersCommand
+		.command('passwd')
+		.description('Set user password')
+		.option('--email <value>', `user's email`)
+		.option('--password <value>', `user's new password`)
+		.action(usersPasswd);
+
+	const rolesCommand = program.command('roles');
+	rolesCommand
+		.command('create')
+		.description('Create a new role')
+		.option('--role <value>', `name for the role`)
+		.option('--admin', `whether or not the role has admin access`)
+		.action(rolesCreate);
+
+	program.command('count <collection>').description('Count the amount of items in a given collection').action(count);
+
+	program
+		.command('bootstrap')
+		.description('Initialize or update the database')
+		.option('--skipAdminInit', 'Skips the creation of the default Admin Role and User')
+		.action(bootstrap);
+
+	await emitAsyncSafe('cli.init.after', { program });
+
+	return program;
+}

.gitignore
@@ -17,3 +17,4 @@ dist
 app/public/img/docs/*
 *.tsbuildinfo
 .e2e-containers.json
+coverage

api/jest.config.js
@@ -1,7 +1,10 @@
+const base = require('../jest.config.js');
+
 require('dotenv').config();
 
 module.exports = {
-	preset: 'ts-jest',
+	...base,
+	roots: ['<rootDir>/src'],
 	verbose: true,
 	setupFiles: ['dotenv/config'],
 	testURL: process.env.TEST_URL || 'http://localhost',

api/src/cli/index.test.ts
@@ -1,0 +1,62 @@
+import { Command } from 'commander';
+import { Extension } from '@directus/shared/types';
+import { createCli } from '.';
+
+jest.mock('../env', () => ({
+	...jest.requireActual('../env').default,
+	LOG_LEVEL: 'silent',
+	EXTENSIONS_PATH: '',
+	SERVE_APP: false,
+}));
+
+jest.mock('@directus/shared/utils/node/get-extensions', () => ({
+	getPackageExtensions: jest.fn(() => Promise.resolve([])),
+	getLocalExtensions: jest.fn(() => Promise.resolve([customCliExtension])),
+}));
+
+jest.mock(`/hooks/custom-cli/index.js`, () => () => customCliHook, { virtual: true });
+
+const customCliExtension: Extension = {
+	path: `/hooks/custom-cli`,
+	name: 'custom-cli',
+	type: 'hook',
+	entrypoint: 'index.js',
+	local: true,
+	root: true,
+};
+
+const beforeHook = jest.fn();
+const afterAction = jest.fn();
+const afterHook = jest.fn(({ program }: { program: Command }) => program.command('custom').action(afterAction));
+const customCliHook = { 'cli.init.before': beforeHook, 'cli.init.after': afterHook };
+
+const writeOut = jest.fn();
+const writeErr = jest.fn();
+
+const setup = async () => {
+	const program = await createCli();
+	program.exitOverride();
+	program.configureOutput({ writeOut, writeErr });
+	return program;
+};
+
+beforeEach(jest.clearAllMocks);
+
+describe('cli hooks', () => {
+	test('should call hooks before and after creating the cli', async () => {
+		const program = await setup();
+
+		expect(beforeHook).toHaveBeenCalledTimes(1);
+		expect(beforeHook).toHaveBeenCalledWith({ program });
+
+		expect(afterHook).toHaveBeenCalledTimes(1);
+		expect(afterHook).toHaveBeenCalledWith({ program });
+	});
+
+	test('should be able to add a custom cli command', async () => {
+		const program = await setup();
+		program.parseAsync(['custom'], { from: 'user' });
+
+		expect(afterAction).toHaveBeenCalledTimes(1);
+	});
+});

api/src/extensions.ts
@@ -37,6 +37,7 @@ import getModuleDefault from './utils/get-module-default';
 
 let extensions: Extension[] = [];
 let extensionBundles: Partial<Record<AppExtensionType, string>> = {};
+const registeredHooks: string[] = [];
 
 export async function initializeExtensions(): Promise<void> {
 	try {
@@ -152,6 +153,13 @@ function registerHooks(hooks: Extension[]) {
 	function registerHook(hook: Extension) {
 		const hookPath = path.resolve(hook.path, hook.entrypoint || '');
 		const hookInstance: HookConfig | { default: HookConfig } = require(hookPath);
+
+		// Make sure hooks are only registered once
+		if (registeredHooks.includes(hookPath)) {
+			return;
+		} else {
+			registeredHooks.push(hookPath);
+		}
 
 		const register = getModuleDefault(hookInstance);
 

jest.config.js
@@ -1,0 +1,19 @@
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleNameMapper: {
+		'@directus/cli(.*)$': `${__dirname}/packages/cli/src/$1`,
+		'@directus/create-directus-project(.*)$': `${__dirname}/packages/create-directus-project/src/$1`,
+		'@directus/drive-s3(.*)$': `${__dirname}/packages/drive-s3/src/$1`,
+		'@directus/drive-gcs(.*)$': `${__dirname}/packages/drive-gcs/src/$1`,
+		'@directus/drive-azure(.*)$': `${__dirname}/packages/drive-azure/src/$1`,
+		'@directus/drive(.*)$': `${__dirname}/packages/drive/src/$1`,
+		'@directus/extension-sdk(.*)$': `${__dirname}/packages/extension-sdk/src/$1`,
+		'@directus/format-title(.*)$': `${__dirname}/packages/format-title/src/$1`,
+		'@directus/gatsby-source-directus(.*)$': `${__dirname}/packages/gatsby-source-directus/src/$1`,
+		'@directus/schema(.*)$': `${__dirname}/packages/schema/src/$1`,
+		'@directus/sdk(.*)$': `${__dirname}/packages/sdk/src/$1`,
+		'@directus/shared(.*)$': `${__dirname}/packages/shared/src/$1`,
+		'@directus/specs(.*)$': `${__dirname}/packages/specs/$1`,
+	},
+};

packages/shared/src/utils/generate-joi.ts
@@ -1,8 +1,13 @@
-import BaseJoi, { AnySchema } from 'joi';
+import BaseJoi, { AnySchema, StringSchema as BaseStringSchema, NumberSchema } from 'joi';
 import { escapeRegExp, merge } from 'lodash';
 import { FieldFilter } from '../types/filter';
 
-const Joi = BaseJoi.extend({
+interface StringSchema extends BaseStringSchema {
+	contains(substring: string): this;
+	ncontains(substring: string): this;
+}
+
+const Joi: typeof BaseJoi = BaseJoi.extend({
 	type: 'string',
 	base: BaseJoi.string(),
 	messages: {
@@ -95,105 +100,109 @@ export function generateJoi(filter: FieldFilter, options?: JoiOptions): AnySchem
 		const operator = Object.keys(value)[0];
 		const compareValue = Object.values(value)[0];
 
+		const getAnySchema = () => schema[key] ?? Joi.any();
+		const getStringSchema = () => (schema[key] ?? Joi.string()) as StringSchema;
+		const getNumberSchema = () => (schema[key] ?? Joi.number()) as NumberSchema;
+
 		if (operator === '_eq') {
-			schema[key] = (schema[key] ?? Joi.any()).equal(compareValue);
+			schema[key] = getAnySchema().equal(compareValue);
 		}
 
 		if (operator === '_neq') {
-			schema[key] = (schema[key] ?? Joi.any()).not(compareValue);
+			schema[key] = getAnySchema().not(compareValue);
 		}
 
 		if (operator === '_contains') {
-			schema[key] = (schema[key] ?? Joi.string()).contains(compareValue);
+			schema[key] = getStringSchema().contains(compareValue);
 		}
 
 		if (operator === '_ncontains') {
-			schema[key] = (schema[key] ?? Joi.string()).ncontains(compareValue);
+			schema[key] = getStringSchema().ncontains(compareValue);
 		}
 
 		if (operator === '_starts_with') {
-			schema[key] = (schema[key] ?? Joi.string()).pattern(new RegExp(`^${escapeRegExp(compareValue as string)}.*`), {
+			schema[key] = getStringSchema().pattern(new RegExp(`^${escapeRegExp(compareValue as string)}.*`), {
 				name: 'starts_with',
 			});
 		}
 
 		if (operator === '_nstarts_with') {
-			schema[key] = (schema[key] ?? Joi.string()).pattern(new RegExp(`^${escapeRegExp(compareValue as string)}.*`), {
+			schema[key] = getStringSchema().pattern(new RegExp(`^${escapeRegExp(compareValue as string)}.*`), {
 				name: 'starts_with',
 				invert: true,
 			});
 		}
 
 		if (operator === '_ends_with') {
-			schema[key] = (schema[key] ?? Joi.string()).pattern(new RegExp(`.*${escapeRegExp(compareValue as string)}$`), {
+			schema[key] = getStringSchema().pattern(new RegExp(`.*${escapeRegExp(compareValue as string)}$`), {
 				name: 'ends_with',
 			});
 		}
 
 		if (operator === '_nends_with') {
-			schema[key] = (schema[key] ?? Joi.string()).pattern(new RegExp(`.*${escapeRegExp(compareValue as string)}$`), {
+			schema[key] = getStringSchema().pattern(new RegExp(`.*${escapeRegExp(compareValue as string)}$`), {
 				name: 'ends_with',
 				invert: true,
 			});
 		}
 
 		if (operator === '_in') {
-			schema[key] = (schema[key] ?? Joi.any()).equal(...(compareValue as (string | number)[]));
+			schema[key] = getAnySchema().equal(...(compareValue as (string | number)[]));
 		}
 
 		if (operator === '_nin') {
-			schema[key] = (schema[key] ?? Joi.any()).not(...(compareValue as (string | number)[]));
+			schema[key] = getAnySchema().not(...(compareValue as (string | number)[]));
 		}
 
 		if (operator === '_gt') {
-			schema[key] = (schema[key] ?? Joi.number()).greater(Number(compareValue));
+			schema[key] = getNumberSchema().greater(Number(compareValue));
 		}
 
 		if (operator === '_gte') {
-			schema[key] = (schema[key] ?? Joi.number()).min(Number(compareValue));
+			schema[key] = getNumberSchema().min(Number(compareValue));
 		}
 
 		if (operator === '_lt') {
-			schema[key] = (schema[key] ?? Joi.number()).less(Number(compareValue));
+			schema[key] = getNumberSchema().less(Number(compareValue));
 		}
 
 		if (operator === '_lte') {
-			schema[key] = (schema[key] ?? Joi.number()).max(Number(compareValue));
+			schema[key] = getNumberSchema().max(Number(compareValue));
 		}
 
 		if (operator === '_null') {
-			schema[key] = (schema[key] ?? Joi.any()).valid(null);
+			schema[key] = getAnySchema().valid(null);
 		}
 
 		if (operator === '_nnull') {
-			schema[key] = (schema[key] ?? Joi.any()).invalid(null);
+			schema[key] = getAnySchema().invalid(null);
 		}
 
 		if (operator === '_empty') {
-			schema[key] = (schema[key] ?? Joi.any()).valid('');
+			schema[key] = getAnySchema().valid('');
 		}
 
 		if (operator === '_nempty') {
-			schema[key] = (schema[key] ?? Joi.any()).invalid('');
+			schema[key] = getAnySchema().invalid('');
 		}
 
 		if (operator === '_between') {
-			const values = compareValue as number[];
-			schema[key] = (schema[key] ?? Joi.number()).greater(values[0]).less(values[1]);
+			const values = compareValue as [number, number];
+			schema[key] = getNumberSchema().greater(values[0]).less(values[1]);
 		}
 
 		if (operator === '_nbetween') {
-			const values = compareValue as number[];
-			schema[key] = (schema[key] ?? Joi.number()).less(values[0]).greater(values[1]);
+			const values = compareValue as [number, number];
+			schema[key] = getNumberSchema().less(values[0]).greater(values[1]);
 		}
 
 		if (operator === '_submitted') {
-			schema[key] = (schema[key] ?? Joi.any()).required();
+			schema[key] = getAnySchema().required();
 		}
 
 		if (operator === '_regex') {
 			const wrapped = compareValue.startsWith('/') && compareValue.endsWith('/');
-			schema[key] = (schema[key] ?? Joi.string()).regex(new RegExp(wrapped ? compareValue.slice(1, -1) : compareValue));
+			schema[key] = getStringSchema().regex(new RegExp(wrapped ? compareValue.slice(1, -1) : compareValue));
 		}
 	}
 

commit 3ad042a1651889bf81420e746ba87e4d4dc412ca

Fix CLI not executing any command
Fixes a regression introduced in #7675

Dockerfile
@@ -34,5 +34,5 @@ RUN npm install
 
 WORKDIR /directus/api
 
-CMD ["sh", "-c", "node ./dist/cli/index.js bootstrap; node ./dist/start.js;"]
+CMD ["sh", "-c", "node ./dist/cli/run.js bootstrap; node ./dist/start.js;"]
 EXPOSE 8055/tcp

api/cli.js
@@ -1,2 +1,2 @@
 #!/usr/bin/env node
-require('./dist/cli/index.js');
+require('./dist/cli/run.js');

api/src/cli/index.ts
@@ -1,7 +1,3 @@
-#!/usr/bin/env node
-
-/* eslint-disable no-console */
-
 import { Command } from 'commander';
 import start from '../start';
 import { emitAsyncSafe } from '../emitter';
@@ -16,15 +12,6 @@ import usersCreate from './commands/users/create';
 import usersPasswd from './commands/users/passwd';
 
 const pkg = require('../../package.json');
-
-if (require.main === module) {
-	createCli()
-		.then((program) => program.parseAsync(process.argv))
-		.catch((err) => {
-			console.error(err);
-			process.exit(1);
-		});
-}
 
 export async function createCli(): Promise<Command> {
 	const program = new Command();

api/src/cli/run.ts
@@ -1,0 +1,9 @@
+import { createCli } from './index';
+
+createCli()
+	.then((program) => program.parseAsync(process.argv))
+	.catch((err) => {
+		// eslint-disable-next-line no-console
+		console.error(err);
+		process.exit(1);
+	});

tests/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-node ./dist/cli/index.js bootstrap;
+node ./dist/cli/run.js bootstrap;
 node ./dist/start.js;